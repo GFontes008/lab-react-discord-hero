5ab2ef2d45b754b1972d37cced16c179028abaab
Ana Rosa <guilherme.fontes98@outlook.com>
2024-05-30 08:16:37 +0100

    lab done

---
+++ discordhero/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="logos">
+      <img
+        src="./src/assets/discord-logo-white.png"
+        className="logo"
+        alt="logo"
+      />
+      <img
+        src="./src/assets/menu-icon.png"
+        className="menu-logo"
+        alt="Menu Logo"
+      />
+      <img
+        src="./src/assets/discord-background.png"
+        className="characters"
+        alt="characters"
+      />
+    </div>
+  );
+}
+
+export default App;
